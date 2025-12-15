c42ad48b682c1f0688023876c68b3b8cc81b6224
isgust <rocha.gustavops@gmail.com>
2025-12-15 11:49:45 -0300

    Feat: Implementar a palavra-chave this e Bound Methods

--- src/main/java/com/ufma/compiladores/lox/Resolver.java
+++ src/main/java/com/ufma/compiladores/lox/Resolver.java
@@ -16,13 +16,22 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitClassStmt(Stmt.Class stmt) {
+        ClassType enclosingClass = currentClass;
+        currentClass = ClassType.CLASS;
+
         declare(stmt.name);
         define(stmt.name);
+
+        beginScope();
+        scopes.peek().put("this", true);
 
         for (Stmt.Function method : stmt.methods) {
             FunctionType declaration = FunctionType.METHOD;
             resolveFunction(method, declaration);
         }
+        endScope();
+
+        currentClass = enclosingClass;
         return null;
     }
 
@@ -31,6 +40,13 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         FUNCTION,
         METHOD
     }
+
+    private enum ClassType {
+        NONE,
+        CLASS
+    }
+
+    private ClassType currentClass = ClassType.NONE;
 
     @Override
     public Void visitBlockStmt(Stmt.Block stmt) {
@@ -229,6 +245,14 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         resolve(expr.value);
         resolve(expr.object);
         return null;
+    }
+
+    @Override
+    public Void visitThisExpr(Expr.This expr) {
+        if (currentClass == ClassType.NONE) {
+            Lox.error(expr.keyword,
+                    "Can't use 'this' outside of a class.");
+            return null;
     }
 
     @Override
