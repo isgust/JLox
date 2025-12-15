4cf2848d53962de1427e4e774211181d0ebd2b69
isgust <rocha.gustavops@gmail.com>
2025-12-15 05:57:03 -0300

    Feat: Implementar atribuição otimizada usando a distância de escopo

--- src/main/java/com/ufma/compiladores/lox/Environment.java
+++ src/main/java/com/ufma/compiladores/lox/Environment.java
@@ -32,6 +32,10 @@ class Environment {
         return ancestor(distance).values.get(name);
     }
 
+    void assignAt(int distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
+    }
+
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
